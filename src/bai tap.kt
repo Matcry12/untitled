cdc84125d0385841dd33889126cf0c876374c5f3
Matcry12 <fdtywadw@gmail.com>
2021-07-29 21:48:32 +0700

    Initial commit

---
+++ src/bai tap.kt
@@ -0,0 +1,19 @@
+
+fun main() {
+    val soPi: Double = 3.14
+    var NamSinh: Int = 2007
+    var Tuoi:Int = 14
+    val Ten_truong: String = "THCS Hong Lac"
+    println("""
+        Bai 1 
+        $soPi
+        $NamSinh
+        $Tuoi
+        $Ten_truong
+        Bai 2 
+        Dien tich hinh chu nhat = ${5*9}
+        Chu vi hình tròn bán kính bằng ${soPi*4}
+        
+    """.trimIndent())
+    //Độ dài cạnh huyền của tam giác vuông cân có cạnh bằng 2
+}
